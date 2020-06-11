337af3d24ecf72b12c0a6c447d406090e2efb46e
katsuki <vagrant@localhost.localdomain>
2020-06-11 18:11:42 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+
+  validates :name,  presence: true, length: {maximum: 30}
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 8}
+
+end
