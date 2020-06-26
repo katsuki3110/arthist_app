0dc5949dc345fee97e733e4d75f001cc4593974d
katsuki <vagrant@localhost.localdomain>
2020-06-26 20:35:01 +0900

    likeテスト修正

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -6,11 +6,11 @@ class LikesControllerTest < ActionDispatch::IntegrationTest
     @user = users(:one)
     @arthist = arthists(:one)
     @sing = @arthist.sings.create!(user_id: @user.id,
-                                   name: "sing_name", 
+                                   name: "sing_name",
                                    link: "https://www.youtube..")
   end
 
-  test "お気に入りする（ログインしていない）" do
+  test "createに失敗する（ログインしていない）" do
     assert_no_difference 'Like.count' do
       post likes_path ,params: {user_id: @user.id,
                                 sing_id: @sing.id}
@@ -18,12 +18,21 @@ class LikesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to new_session_path
   end
 
-  test "お気に入り解除する（ログインしていない）" do
+  test "destroyに失敗する（ログインしていない）" do
     @user.likes.create(sing_id: @sing.id)
     assert_no_difference 'Like.count' do
       delete like_path(@sing)
     end
     assert_redirected_to new_session_path
+  end
+
+  test "destroyに失敗する（存在しないsing）" do
+    log_in_as @user
+    @user.likes.create(sing_id: @sing.id)
+    assert_no_difference 'Like.count' do
+      delete like_path(Sing.last.id + 1)
+    end
+    assert_redirected_to root_path
   end
 
 
