a446a3955c58d8bb6473452ffcae583a03111e43
katsuki <vagrant@localhost.localdomain>
2020-06-26 20:17:33 +0900

    userテスト修正

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,6 +2,12 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
-  
+    test "createに失敗する（validationエラー）" do
+      post users_path, params: {user: {name:  "user_name",
+                                        email: "",
+                                        password:              "password",
+                                        password_confirmation: "invalid"}}
+      assert_template 'users/new'
+    end
 
 end
