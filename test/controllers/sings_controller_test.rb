46ae5e20f79bf210614d4d3db2cfef62341fb3aa
katsuki <vagrant@localhost.localdomain>
2020-06-26 20:30:41 +0900

    singテスト修正

--- test/controllers/sings_controller_test.rb
+++ test/controllers/sings_controller_test.rb
@@ -5,22 +5,50 @@ class SingsControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:one)
     @other_user = users(:two)
-    @sing = sings(:one)
+    @arthist = arthists(:one)
+    @sing = @arthist.sings.create!(user_id: @user.id,
+                                   name: "sing_name",
+                                   link: "sing_link")
     @other_sing = sings(:two)
   end
 
-  test "sing削除に失敗する（ログインしていない）" do
+  test "index_月初処理(デビューしているアーティストを削除)" do
+    @arthist.debut = true
+    @arthist.save
+    if Date.today == Time.current.beginning_of_month
+      assert_difference 'Arthist.count', -1 do
+        assert_difference 'Sing.count', -1 do
+          get sings_path
+        end
+      end
+    end
+  end
+
+  test "destroyに失敗する（ログインしていない）" do
     assert_no_difference 'Sing.count' do
       delete sing_path(@sing)
     end
     assert_redirected_to new_session_path
   end
 
-  test "sing削除に失敗する（他ユーザー投稿のsing）" do
+  test "destroy_月初処理(デビューしているアーティストを削除)" do
+    @arthist.debut = true
+    @arthist.save
+    if Date.today == Time.current.beginning_of_month
+      assert_difference 'Arthist.count', -1 do
+        assert_difference 'Sing.count', -1 do
+          delete sings_path(@sing)
+        end
+      end
+    end
+  end
+
+  test "destroyに失敗する（他ユーザー投稿のsing）" do
     log_in_as @user
     assert_no_difference 'Sing.count' do
       delete sing_path(@other_sing)
     end
+    assert_redirected_to root_path
   end
 
 end
