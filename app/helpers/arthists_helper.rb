b4b6a9075de1e0114d9d44898459c13e57355958
katsuki <vagrant@localhost.localdomain>
2020-07-05 23:36:21 +0900

    youtube instagram リンク取得位置追加

--- app/helpers/arthists_helper.rb
+++ app/helpers/arthists_helper.rb
@@ -1,13 +1,18 @@
 module ArthistsHelper
 
   def video_judge(sing)
-    if sing.link[12] == "y"
+    if sing.link[12] == "y" || sing.link[8] == "y"
       #youtubeを投稿
       sing.link = sing.link.last(11)
       sing.video_flg = 1
     elsif sing.link[12] == "i"
       #instagramを投稿
-      sing.link = sing.link[28,11]
+      if sing.link[26] == "p"
+        sing.link = sing.link[28,11]
+      elsif sing.link[26, 2] == "tv"
+        #IGTV
+        sing.link = sing.link[29,11]
+      end
       sing.video_flg = 2
     else
       flash[:danger] = "指定のリンクはシェア出来ません"
